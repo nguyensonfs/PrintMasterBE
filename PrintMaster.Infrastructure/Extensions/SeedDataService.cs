bf2592154a3e3876197bd6ad9ce2606506f665be
nguyensonfs <wingc.dev@gmail.com>
2024-09-03 16:26:01 +0700

    update seed data

--- PrintMaster.Infrastructure/Extensions/SeedDataService.cs
+++ PrintMaster.Infrastructure/Extensions/SeedDataService.cs
@@ -66,10 +66,170 @@ namespace PrintMaster.Infrastructure.Extensions
                 _context.ResourceTypes.AddRange(
                     new ResourceType()
                     {
-                        NameOfResourceType = "Machines",
+                        NameOfResourceType = "Consumable",
+                        Id = Guid.NewGuid(),
+                        IsDeleted = false,
+                    },
+                    new ResourceType()
+                    {
+                        NameOfResourceType = "Non-consumable",
                         Id = Guid.NewGuid(),
                         IsDeleted = false,
                     }
+                    );
+                _context.SaveChanges();
+            }
+            if (!_context.Resources.Any())
+            {
+                var resourceTypeConsumable = _context.ResourceTypes.FirstOrDefault(r => r.NameOfResourceType == "Consumable");
+                var resourceTypeNonConsumable = _context.ResourceTypes.FirstOrDefault(r => r.NameOfResourceType == "Non-consumable");
+                _context.Resources.AddRange(
+                    new Resource()
+                    {
+                        ResourceName = "Machinery",
+                        ResourceTypeId = resourceTypeNonConsumable.Id,
+                        Image = "https://www.anlocviet.vn/upload/images/van-phong-pham-quan-hai-ba-trung-3.jpg",
+                        AvailableQuantity = 350,
+                        Id = Guid.NewGuid(),
+                        IsDeleted = false,
+                    },
+                    new Resource()
+                    {
+                        ResourceName = "Office supplies",
+                        ResourceTypeId = resourceTypeConsumable.Id,
+                        Image = "https://png.pngtree.com/thumb_back/fw800/background/20230722/pngtree-metallic-machinery-a-3d-rendered-illustration-for-your-background-image_3781977.jpg",
+                        AvailableQuantity = 10,
+                        Id = Guid.NewGuid(),
+                        IsDeleted = false,
+                    }
+                    );
+                _context.SaveChanges();
+            }
+            if (!_context.ResourceProperties.Any())
+            {
+                var resourceMachinery = _context.Resources.FirstOrDefault(r => r.ResourceName == "Machinery");
+                var resourceOfficeSupplies = _context.Resources.FirstOrDefault(r => r.ResourceName == "Office supplies");
+                _context.ResourceProperties.AddRange(
+                    new ResourceProperty()
+                    {
+                        ResourcePropertyName = "Giấy",
+                        Quantity = 300,
+                        ResourceId = resourceOfficeSupplies.Id,
+                        Id = Guid.NewGuid(),
+                        IsDeleted = false,
+                    },
+                    new ResourceProperty()
+                    {
+                        ResourcePropertyName = "Mực",
+                        Quantity = 50,
+                        ResourceId = resourceOfficeSupplies.Id,
+                        Id = Guid.NewGuid(),
+                        IsDeleted = false,
+                    },
+                    new ResourceProperty()
+                    {
+                        ResourcePropertyName = "Máy in",
+                        Quantity = 10,
+                        ResourceId = resourceMachinery.Id,
+                        Id = Guid.NewGuid(),
+                        IsDeleted = false,
+                    }
+                    );
+                _context.SaveChanges();
+            }
+            if (!_context.ResourcePropertyDetails.Any())
+            {
+                var resourcePrinterPaper = _context.ResourceProperties.FirstOrDefault(r => r.ResourcePropertyName == "Giấy");
+                var resourcePrinterInk = _context.ResourceProperties.FirstOrDefault(r => r.ResourcePropertyName == "Mực");
+                var resourcePrinter = _context.ResourceProperties.FirstOrDefault(r => r.ResourcePropertyName == "Máy in");
+                _context.ResourcePropertyDetails.AddRange(
+                #region Giấy
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Giấy A4",
+                        Quantity = 100,
+                        ResourcePropertyId = resourcePrinterPaper.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 1000,
+                        IsDeleted = false,
+                    },
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Giấy A3",
+                        Quantity = 100,
+                        ResourcePropertyId = resourcePrinterPaper.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 1000,
+                        IsDeleted = false,
+                    },
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Giấy A2",
+                        Quantity = 100,
+                        ResourcePropertyId = resourcePrinterPaper.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 1000,
+                        IsDeleted = false,
+                    },
+                #endregion
+                #region Mực
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Mực Xanh",
+                        Quantity = 5,
+                        ResourcePropertyId = resourcePrinterInk.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 1000,
+                        IsDeleted = false,
+                    },
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Mực Đỏ",
+                        Quantity = 10,
+                        ResourcePropertyId = resourcePrinterInk.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 1000,
+                        IsDeleted = false,
+                    },
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Mực Tím",
+                        Quantity = 10,
+                        ResourcePropertyId = resourcePrinterInk.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 1000,
+                        IsDeleted = false,
+                    },
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Mực Vàng",
+                        Quantity = 25,
+                        ResourcePropertyId = resourcePrinterInk.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 1000,
+                        IsDeleted = false,
+                    },
+                #endregion
+                #region Máy in
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Máy in 3D",
+                        Quantity = 5,
+                        ResourcePropertyId = resourcePrinter.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 24000000,
+                        IsDeleted = false,
+                    },
+                    new ResourcePropertyDetail()
+                    {
+                        Name = "Máy in laser",
+                        Quantity = 5,
+                        ResourcePropertyId = resourcePrinter.Id,
+                        Id = Guid.NewGuid(),
+                        Price = 240000000,
+                        IsDeleted = false,
+                    }
+                    #endregion
                     );
                 _context.SaveChanges();
             }
@@ -79,9 +239,12 @@ namespace PrintMaster.Infrastructure.Extensions
                 var managerRole = _context.Roles.FirstOrDefault(r => r.RoleCode == "Manager");
                 var employeeRole = _context.Roles.FirstOrDefault(r => r.RoleCode == "Employee");
                 var leaderRole = _context.Roles.FirstOrDefault(r => r.RoleCode == "Leader");
+                var designerRole = _context.Roles.FirstOrDefault(r => r.RoleCode == "Designer");
+                var deliverRole = _context.Roles.FirstOrDefault(r => r.RoleCode == "Deliver");
 
                 var salesTeam = _context.Teams.FirstOrDefault(t => t.Name == "Sales");
                 var technicalTeam = _context.Teams.FirstOrDefault(t => t.Name == "Technical");
+                var deliveryTeam = _context.Teams.FirstOrDefault(t => t.Name == "Delivery");
 
                 var adminUser = new User
                 {
@@ -139,7 +302,7 @@ namespace PrintMaster.Infrastructure.Extensions
                     DateOfBirth = new DateTime(1990, 5, 15),
                     CreateTime = DateTime.Now,
                     TeamId = technicalTeam?.Id,
-                    PhoneNumber = "987654321",
+                    PhoneNumber = "0987654321",
                     Permissions = managerRole != null ? new List<Permission>
                     {
                         new() { Role = managerRole },
@@ -147,7 +310,90 @@ namespace PrintMaster.Infrastructure.Extensions
                     } : null
                 };
 
-                _context.Users.AddRange(adminUser, saleManagerUser, technicalManagerUser);
+                var deliverManagerUser = new User
+                {
+                    Id = Guid.NewGuid(),
+                    UserName = "deliverleader",
+                    Password = SecurityUtilities.HashPassword("deliverleader123"),
+                    Email = "deliverleader@gmail.com",
+                    FullName = "Deliver Manager",
+                    Avatar = "https://i.pngimg.me/thumb/f/720/a43893ada7.jpg",
+                    Gender = GenderEnum.Female,
+                    Status = UserStatus.Activated,
+                    DateOfBirth = new DateTime(1990, 5, 15),
+                    CreateTime = DateTime.Now,
+                    TeamId = deliveryTeam?.Id,
+                    PhoneNumber = "0987654333",
+                    Permissions = managerRole != null ? new List<Permission>
+                    {
+                        new() { Role = managerRole },
+                        new() { Role = leaderRole }
+                    } : null
+                };
+
+                var designerUser = new User
+                {
+                    Id = Guid.NewGuid(),
+                    UserName = "designer",
+                    Password = SecurityUtilities.HashPassword("designer123"),
+                    Email = "designer@gmail.com",
+                    FullName = "Designer",
+                    Avatar = "https://i.pngimg.me/thumb/f/720/a43893ada7.jpg",
+                    Gender = GenderEnum.Female,
+                    Status = UserStatus.Activated,
+                    DateOfBirth = new DateTime(1990, 5, 15),
+                    CreateTime = DateTime.Now,
+                    TeamId = technicalTeam?.Id,
+                    PhoneNumber = "0987654323",
+                    Permissions = designerRole != null ? new List<Permission>
+                    {
+                        new() { Role = designerRole },
+                        new() { Role = employeeRole }
+                    } : null
+                };
+
+                var deliverUser = new User
+                {
+                    Id = Guid.NewGuid(),
+                    UserName = "deliver",
+                    Password = SecurityUtilities.HashPassword("deliver123"),
+                    Email = "deliver@gmail.com",
+                    FullName = "Deliver",
+                    Avatar = "https://i.pngimg.me/thumb/f/720/a43893ada7.jpg",
+                    Gender = GenderEnum.Female,
+                    Status = UserStatus.Activated,
+                    DateOfBirth = new DateTime(1990, 5, 15),
+                    CreateTime = DateTime.Now,
+                    TeamId = deliveryTeam?.Id,
+                    PhoneNumber = "0987654324",
+                    Permissions = deliverRole != null ? new List<Permission>
+                    {
+                        new() { Role = deliverRole },
+                        new() { Role = employeeRole }
+                    } : null
+                };
+
+                var saleUser = new User
+                {
+                    Id = Guid.NewGuid(),
+                    UserName = "saler",
+                    Password = SecurityUtilities.HashPassword("saler123"),
+                    Email = "saler@gmail.com",
+                    FullName = "Saler",
+                    Avatar = "https://i.pngimg.me/thumb/f/720/a43893ada7.jpg",
+                    Gender = GenderEnum.Female,
+                    Status = UserStatus.Activated,
+                    DateOfBirth = new DateTime(1990, 5, 15),
+                    CreateTime = DateTime.Now,
+                    TeamId = salesTeam?.Id,
+                    PhoneNumber = "0987654328",
+                    Permissions = employeeRole != null ? new List<Permission>
+                    {
+                        new() { Role = employeeRole }
+                    } : null
+                };
+
+                _context.Users.AddRange(adminUser, saleManagerUser, technicalManagerUser, designerUser, deliverUser, saleUser, deliverManagerUser);
 
                 _context.SaveChanges();
             }
@@ -161,7 +407,7 @@ namespace PrintMaster.Infrastructure.Extensions
                         FullName = "Trần Minh Quân",
                         PhoneNumber = "0328033706",
                         Address = "Bình Dương, Việt Nam",
-                        Email = "minhquan@gmail.com"
+                        Email = "minhquan@gmail.com",
                     }, new Customer
                     {
                         Id = Guid.NewGuid(),
@@ -196,6 +442,17 @@ namespace PrintMaster.Infrastructure.Extensions
                         Email = "nguyenthuy@gmail.com"
                     });
                 _context.SaveChanges();
+            };
+            if (!_context.ShippingMethods.Any())
+            {
+                _context.ShippingMethods.AddRange(
+                    new ShippingMethod
+                    {
+                        Id = Guid.NewGuid(),
+                        IsDeleted = false,
+                        ShippingMethodName = "Chuyển Phát Nhanh"
+                    }
+                    );
             }
         }
     }
